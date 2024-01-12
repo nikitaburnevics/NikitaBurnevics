8affaa91155e5c883bdcbeef1fb93fd1e2b47a0d
nikitaburnevics <nikita.burnevics@svg.lv>
2024-01-12 14:37:20 +0200

    Update Ievarijums-spec-Nikita-Justs.py

--- Ievarijums-spec-Nikita-Justs.py
+++ Ievarijums-spec-Nikita-Justs.py
@@ -9,3 +9,14 @@ kopsumma = abolu_daudzums*cukura_daudzums_vajadz*cukura_cena #specifikācijā do
 kopsumma = round(kopsumma, 2) #rezultāts noapaļots līdz simtdaļām 
 
 print('-------------------------------\nGala izmaksas ir ', kopsumma, "€", "\n-------------------------------") #"skaists" atbildes noformējums
+
+cukura_cena = 2
+
+abolu_daudzums = float(input('Ievadiet cik jums ir āboli (Kg) ja nav pilnds skaitlis atdalītājs ir ".":\n')) #lietotājs ievada cik viņam ir āboli
+
+cukura_daudzums_vajadz = float(input("Ievadiet cik jums ir nepieciešams cukurs (g/kg):\n")) #lietotājs ievada cik viņam ir nepieciešams cukurs uz 1 kg ābolu
+
+kopsumma = abolu_daudzums*cukura_daudzums_vajadz*cukura_cena #specifikācijā dotā funkcija cenas aprēķināšanai
+kopsumma = round(kopsumma, 2) #rezultāts noapaļots līdz simtdaļām 
+
+print('-------------------------------\nGala izmaksas ir ', kopsumma, "€", "\n-------------------------------") #"skaists" atbildes noformējums
