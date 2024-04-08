03c33395c486a39081e170261247f7bc0a26fa67
nikitaburnevics <nikita.burnevics@svg.lv>
2024-04-08 12:00:22 +0300

    d

--- programmesana/programmesana/praktiskie_DARB!/rekini_Markuss_Nikita.py
+++ programmesana/programmesana/praktiskie_DARB!/rekini_Markuss_Nikita.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import csv
 
 class rekins:
     def __init__(self):
@@ -7,7 +8,7 @@ class rekins:
         self.teksts = input('Ievadiet veltījuma tekstu: ')
         self.platums, self.garums, self.augstums = int(input('Ievadiet izmēru milimetros\nPlatums: ')), int(input('Garums: ')), int(input('Augstums: '))
         self.materiala_cena = float(input('Ievadiet kokmateriāla cenu (EUR/m^2)'))
-        
+
     def display(self):
         current_time = self.laiks.strftime("%H:%M:%S")
         print(f"Rēķina izveidošanas laiks: {current_time}")
@@ -17,6 +18,19 @@ class rekins:
         pvn_summa = (produkta_cena + 15)*21/100
         print('Cena bez pvn:',produkta_cena)
         print('Cena ar pvn:',pvn_summa+produkta_cena)
+                               
+rekins1 = rekins()                        
+gals = rekins1.display()    
 
-rekins1 = rekins()
-rekins1.display()
+dati = 'dati.csv'
+
+try:
+    with open(dati, 'w', newline='') as jaunie_fails:
+        rakstitajs = csv.DictWriter(jaunie_fails, fieldnames=[gals])
+        rakstitajs.writeheader()
+        rakstitajs.writerows(dati)
+
+except FileNotFoundError:
+    print(f"Kļūda: Fails '{dati}' nav atrasts.")
+except Exception as e:
+    print(f"Kļūda: Neparedzēta kļūda - {e}")
